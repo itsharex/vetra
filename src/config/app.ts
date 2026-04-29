1eb8b8d342d88f3ca6a63681ff00553f070bac6b
llds <858400061@qq.com>
2026-04-29 20:54:32 +0800

    chore: release v0.2.0

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,5 +3,5 @@ export const appConfig = {
   authorUrl: 'https://llds.cloud',
   projectTemplate: 'Vetra',
   projectTemplateUrl: 'https://github.com/llds66/vetra',
-  version: 'v0.1.0',
+  version: 'v0.2.0',
 } as const
